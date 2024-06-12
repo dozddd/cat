a238f112b022a424f2b86b790363a3ddfedc1b56
dozddd <polinahandogina86@gmail.com>
2024-06-13 00:18:37 +0500

    my commit

---
+++ cat/mycat.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Начало работы программы")
+	checkArgs()
+	fmt.Println("Конец работы программы")
+}
+
+func checkArgs() {
+	numberOfArgs := len(os.Args)
+	if numberOfArgs == 2 {
+		readText()
+	} else {
+		fmt.Println("Укажите только один файл")
+	}
+}
+
+func readText() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Проверьте правильность указанных файлов")
+	} else {
+		fmt.Println("Чтение файла...")
+		Data := bufio.NewScanner(file)
+		for Data.Scan() {
+			fmt.Printf("%s\n", Data.Text())
+		}
+	}
+}
